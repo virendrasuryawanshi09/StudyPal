76acf3e7c4abf5e4a33e524fa416697f19677ee9
Virendra Suryawanshi <virendrasuryawanshi805@gmail.com>
2025-12-16 10:34:47 +0530

    Created basic server.js

---
+++ backend/server.js
@@ -0,0 +1,63 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import errorHandler from './middleware/errorHandler.js';
+import { connect } from 'http2';
+import { error } from 'console';
+
+const__filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+
+connectDB();
+
+app.use(
+    cors({
+        origin:"*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
+    })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+
+//routes
+app.use(errorHandler);
+
+app.use((req, res) => {
+    res.status(404).json({ message: 'Route not found', status: 404 });
+
+})
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running in ${process.env.NODE_ENV} node on port ${PORT}`);
+});
+
+process.on('unhandledRejection', (err) => {
+    console.error(`Error: ${err.message}`);
+    console.log('Shutting down the server due to Unhandled Promise Rejection');
+    server.close(() => {
+        process.exit(1);
+    });
+});
+process.on('uncaughtException', (err) => {
+    console.error(`Error: ${err.message}`);
+    console.log('Shutting down the server due to Uncaught Exception');
+    server.close(() => {
+        process.exit(1);
+    }
+    );
+});
