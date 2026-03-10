58fc36739181fbda72f721f7cf394e23583e5132
virendra <virendrasuryawanshi901@gmail.com>
2026-03-10 15:41:04 +0530

    feat: initialize backend server with Express, database connection, CORS, and API routes.

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,10 @@ connectDB();
 
 app.use(
     cors({
-        origin: "*",
+        origin: [
+            "http://localhost:5173",
+            "https://study-pal-eta-eosin.vercel.app"
+        ],
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
